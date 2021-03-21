e0f39d1ae6424093616e489cd3a32d93eb8bbc79
Phan Trinh <phan181299@gmail.com>
2021-03-21 14:01:30 -0400

    Running untested code

--- dtree.py
+++ dtree.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy
 from scipy.stats import mode, entropy
+import pandas as pd
+from pprint import pprint
 
 class Node:
     def __init__(self, label, is_leaf=False, min_sample_leaf=1):
@@ -10,20 +12,28 @@ class Node:
         self.min_sample_leaf = min_sample_leaf
         self.attr_idx = None
         self.val = None
-        self.eval_funcs = []
+        self.eval_func = None
         self.type = None # 'cont' or 'cat'
     
-    def train(self, X, y: np.ndarray, properties):
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+    
+    def __str__(self) -> str:
+        return str(self.__dict__)
+    
+    def _train(self, X, y: np.ndarray, properties):
         """
         X: attrs
         y: classes
         properties: list of propaerties of each attr
             ['cat', 'cont', 'cat', 'cont']
         """
+        print(properties)
+        assert X.shape[0] == y.size
         classes, counts = np.unique(y, return_counts=True)
         # Apply root to be labelled as the most popular class
         most_pop = mode(y)[0][0]
-        if len(y) <= self.min_sample_leaf: return Node(most_pop, is_leaf=True)
+        if len(y) <= self.min_sample_leaf or classes.size == 1 or len(properties) <= 0: return Node(most_pop, is_leaf=True)
         I = entropy(counts/y.size, base=2)
         """
         For each column, calculate the entropy of the column of data
@@ -45,7 +55,7 @@ class Node:
                     I_res += val_count/ y.size * cond_class_entropy
             elif properties[i] == 'cont':
                 col_sort = np.sort(np.unique(col))
-                split_points = (col_sort[1::] + col_sort[0::]) * 0.5
+                split_points = (col_sort[1::] + col_sort[0:-1]) * 0.5
                 possible_i_res = np.zeros_like(split_points)
                 index = 0
                 for sp in split_points:
@@ -55,6 +65,8 @@ class Node:
                         cond_class_entropy = entropy(cond_counts/D.size, base=2)
                         possible_i_res[index] += D.size/ y.size * cond_class_entropy
                     index += 1
+                print(col)
+                print(possible_i_res)
                 I_res_idx= np.argmin(possible_i_res)
                 I_res = possible_i_res[I_res_idx]
                 possible_known_vals = np.array([split_points[I_res_idx]])
@@ -63,20 +75,42 @@ class Node:
                 known_vals = possible_known_vals
                 IG = I - I_res
         root = Node(None)
+        root.type = properties[best_attr]
+        root.attr_idx = best_attr
         if root.type == 'cat':
             for val in known_vals:
-                root.type = properties[best_attr]
-                root.attr_idx = best_attr
-                x_sub, y_sub = X[:, X[best_attr] == val], y[X[best_attr] == val]
-                branch = Node(None)
+                x_sub, y_sub = X[X[:, best_attr] == val], y[X[:, best_attr] == val]
                 if y_sub.size <= 0:
+                    root.children.append(Node(most_pop, is_leaf=True))
+                else:
+                    root.children.append(root._train(
+                                        np.delete(x_sub, root.attr_idx, axis=1), 
+                                        y_sub, 
+                                        properties=properties[:best_attr] + properties[best_attr+1:]))
                     
         elif root.type == 'cont':
             val = known_vals[0] # single Y/N split
-            x_val_hi, y_val_hi = X[:, X[best_attr] > val], y[X[best_attr] > val]
-            x_val_lo, y_val_lo = X[:, X[best_attr] <= val], y[X[best_attr] <= val]
-            
+            x_val_hi, y_val_hi = X[X[:, best_attr] > val], y[X[:, best_attr] > val]
+            x_val_lo, y_val_lo = X[X[:, best_attr] <= val], y[X[:, best_attr] <= val]
+            if y_val_hi.size <= 0:
+                root.children.append(Node(most_pop, is_leaf=True))
+            else:
+                root.children.append(root._train(
+                                        np.delete(x_val_hi, root.attr_idx, axis=1), 
+                                        y_val_hi, 
+                                        properties=properties[:best_attr] + properties[best_attr+1:]))
+            if y_val_lo.size <= 0:
+                root.children.append(Node(most_pop, is_leaf=True))
+            else:
+                root.children.append(root.train(
+                                        np.delete(x_val_lo, 1, axis=1), 
+                                        y_val_lo, 
+                                        properties=properties[:best_attr] + properties[best_attr+1:]))
         return root
+
+    @classmethod
+    def train(cls, X, y, properties):
+        return Node(None)._train(X, y, properties)
 
             
 
@@ -86,8 +120,14 @@ class DecisionTreeClassifier:
 
     def __init__(self, min_sample_leaf=1) -> None:
         self.min_sample_leaf = min_sample_leaf
-        self.root = None
+        self.root = Node(None)
     
 
     def predict(self, X):
         pass
+
+if __name__ == "__main__":
+    df = pd.read_csv("data.csv")
+    X = df.drop("play",axis=1).to_numpy()
+    Y = df.play.to_numpy()
+    pprint(Node.train(X, Y, ["cat", "cont", 'cont', 'cat']).__dict__)
